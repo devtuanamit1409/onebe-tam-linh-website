8044e108b1e2848ff51f1a0d6dbe7c8b510b633c
Tuan Nguyen AMIT <nguyentuanamit@gmail.com>
2024-05-23 13:19:16 +0700

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
             Giới Thiệu Về Chúng Tôi
           </h2>
           <div className="flex justify-center">
-            <div className="md:w-[1038px] z-50">
-              <p className="text-[#111928] py-[24px] text-start md:text-center text-[20px] font-medium">
+            <div className="laptop:w-[1038px] z-50">
+              <p className="text-[#111928] py-[24px] text-start laptop:text-center text-[20px] font-medium">
                 Thành lập từ năm 2013, Công ty TNHH Kỹ thuật NTS định hướng trở
                 thành nhà cung cấp hàng đầu cho các giải pháp kỹ thuật công
                 trình. Theo đó những lĩnh vực chính mà NTS theo đuổi một cách
@@ -38,7 +38,7 @@ export default function Home() {
                 </button>
               </div>
               <div className="grid grid-cols-12 gap-[24px] pt-[32px]">
-                <div className="col-span-12 md:col-span-4">
+                <div className="col-span-12 laptop:col-span-4">
                   <div className="sp  overflow-hidden h-[437px]">
                     <div className="p-[32px] ">
                       <div className="bg-[#fff] w-[60px] h-[60px] rounded-[100px] flex justify-center items-center mx-0">
@@ -65,7 +65,7 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div className="col-span-12 md:col-span-4">
+                <div className="col-span-12 laptop:col-span-4">
                   <div className="dv  overflow-hidden h-[437px]">
                     <div className="p-[32px] ">
                       <div className="bg-[#fff] w-[60px] h-[60px] rounded-[100px] flex justify-center items-center mx-0">
@@ -92,7 +92,7 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div className="col-span-12 md:col-span-4">
+                <div className="col-span-12 laptop:col-span-4">
                   <div className="du  overflow-hidden h-[437px]">
                     <div className="p-[32px] ">
                       <div className="bg-[#fff] w-[60px] h-[60px] rounded-[100px] flex justify-center items-center mx-0">
@@ -124,13 +124,13 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="h-[0px] md:h-[110px]"></div>
+        <div className="h-[0px] laptop:h-[110px]"></div>
       </div>
       <div className="section-member py-[120px]">
         <div className="flex justify-center">
           <div className="container">
             <div className="grid grid-cols-12 gap-8 pt-[5%]">
-              <div className="col-span-12 md:col-span-6 relative">
+              <div className="col-span-12 laptop:col-span-6 relative">
                 <div className="h-[417px] w-[356px] card-member relative z-50">
                   <SlideMember />
                 </div>
@@ -143,7 +143,7 @@ export default function Home() {
                   height={100}
                 />
               </div>
-              <div className="col-span-12 md:col-span-6">
+              <div className="col-span-12 laptop:col-span-6">
                 <div className="flex justify-end">
                   <div>
                     <h2 className="font-bold text-[48px]">
@@ -169,14 +169,14 @@ export default function Home() {
       </div>
       <div className="pt-[120px]">
         <div className="flex justify-center">
-          <div className="md:w-[1038px]">
+          <div className="laptop:w-[1038px]">
             <div className="text-center">
               <h3 className="text-[#28A645] text-[20px] font-bold">DỰ ÁN</h3>
               <h2 className="text-[#111928] text-[40px] font-bold py-[16px]">
                 Công Trình Đã Thực Hiện
               </h2>
               <div className="flex justify-center">
-                <p className="text-[#637381] text-[20px] md:w-[572px]">
+                <p className="text-[#637381] text-[20px] laptop:w-[572px]">
                   This is a short description about this content.This is a short
                   description about this content.
                 </p>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -130,7 +130,8 @@ const Header: React.FC = () => {
               <li
                 key={item.key}
                 className={`border-b-2 border-transparent flex items-center
-                 `}>
+                 `}
+              >
                 <div
                   onClick={(event) => {
                     event.stopPropagation();
@@ -140,7 +141,8 @@ const Header: React.FC = () => {
                   ${
                     activeKey === item.key ? "text-[#28A645]" : "text-[#3B559E]"
                   }
-                  `}>
+                  `}
+                >
                   {item.label}
                 </div>
               </li>
@@ -162,7 +164,8 @@ const Header: React.FC = () => {
         <div className=" w-8 h-8 px-[0.85px] py-[6.30px] justify-center items-center ">
           <button
             className="w-[30.30px] h-[19.40px] relative"
-            onClick={toggleMenu}>
+            onClick={toggleMenu}
+          >
             <IconMenu />
           </button>
           <MobileMenu isOpen={isOpen} toggleMenu={toggleMenu} />
