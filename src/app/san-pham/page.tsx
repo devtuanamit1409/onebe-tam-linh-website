a7b00e6d8f0fcf8e2bea88ec66b7f2739ed51aee
Le The Thuoc <lethethuoc@amitgroup.vn>
2024-05-27 10:13:56 +0700

    feat: done page san pham

--- src/app/san-pham/page.tsx
+++ src/app/san-pham/page.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import { Collapse, Descriptions } from "antd";
 import CollapsePanel from "antd/es/collapse/CollapsePanel";
 import PageMenu from "@/components/PageMenu";
+import BoxTinTuc from "@/components/BoxTinTuc/BoxTinTuc";
+import ContactEnd from "@/components/ContactEnd/ContactEnd";
+import IconArrowRight from "@/components/icons/IconArrowRight";
 
 const page = () => {
   const menuItem = [
@@ -98,6 +101,44 @@ const page = () => {
       ],
     },
   ];
+  const data_tin_tuc = [
+    {
+      url: "/images/tin-tuc/tin-tuc-1.jpg",
+      title: "Meet AutoManage, the best AI management tools",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      url: "/images/tin-tuc/tin-tuc-2.jpg",
+      title: "How to earn more money as a wellness coach",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      url: "/images/tin-tuc/tin-tuc-3.jpg",
+      title: "The no-fuss guide to upselling and cross selling",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      url: "/images/tin-tuc/tin-tuc-1.jpg",
+      title: "Meet AutoManage, the best AI management tools",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      url: "/images/tin-tuc/tin-tuc-2.jpg",
+      title: "How to earn more money as a wellness coach",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      url: "/images/tin-tuc/tin-tuc-3.jpg",
+      title: "The no-fuss guide to upselling and cross selling",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+  ];
   return (
     <div>
       <div className="relative w-full h-[18.5%] min-h-[682px] overflow-hidden">
@@ -118,6 +159,24 @@ const page = () => {
           </p>
         </div>
         <PageMenu menu={menuItem} />
+      </div>
+      <div className="bg-[#F3F6FE] py-[80px]">
+        <div className="container">
+          <div className="inline-flex justify-between items-center w-full py-2 pb-[40px]">
+            <h2 className="text-black text-[32px] font-bold capitalize leading-[51.20px]">
+              Tin Tức
+            </h2>
+            <Link
+              href={"/"}
+              className="text-center text-indigo-800 text-base font-medium leading-normal inline-flex gap-2.5">
+              Tới trang tin tức <IconArrowRight />
+            </Link>
+          </div>
+          <BoxTinTuc data={data_tin_tuc.slice(0, 3)} />
+        </div>
+      </div>
+      <div className="container">
+        <ContactEnd />
       </div>
     </div>
   );
