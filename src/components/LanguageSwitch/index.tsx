d11d10766339e9158e727d8c3ff0f536f69a5dd0
Tuan Nguyen AMIT <nguyentuanamit@gmail.com>
2024-06-13 13:09:59 +0700

    change

--- src/components/LanguageSwitch/index.tsx
+++ src/components/LanguageSwitch/index.tsx
@@ -6,30 +6,42 @@ import IconGlobe from "../icons/IconGlobe";
 import { type Locale } from "../../locales";
 import { useLocale } from "next-intl";
 import { usePathname, useRouter } from "next/navigation";
-import CustomLink from "../CustomLink/CustomLink";
 
 interface LanguageSwitchProps {
   initialLanguage: string;
 }
+
 const LanguageSwitch: React.FC = () => {
   const locale = useLocale() as Locale;
   const router = useRouter();
   const pathname = usePathname();
 
   const slugMap: { [key: string]: string } = {
-    "/en": "/vi",
-    "/vi": "/en",
+    "/en": "/",
+    "/": "/en",
     "/en/neww-post": "/vi/bai-viet",
     "/vi/bai-viet": "/en/neww-post",
   };
+
   function handleLocaleChange(newLocale: Locale): void {
+    console.log("Current locale:", locale);
+    console.log("New locale:", newLocale);
+    console.log("Current pathname:", pathname);
+
     if (newLocale === locale) {
+      console.log("Same locale selected, no change.");
       return;
     }
+
     document.cookie = `NEXT_LOCALE=${newLocale}; path=/; max-age=31536000; SameSite=Lax`;
+
     const newUrl = slugMap[pathname] || pathname;
+    console.log("New URL to navigate:", newUrl);
     router.replace(newUrl);
   }
+  useEffect(() => {
+    console.log("Current pathname updated to:", pathname);
+  }, [pathname]);
 
   const menu = (
     <Menu>
