d3515112bd3ffc77b7a74222c8d21a3aa13db526
Le The Thuoc <lethethuoc@amitgroup.vn>
2024-11-05 16:36:47 +0700

    fix: fix slider bannder home

--- src/components/Slidehome/SlideHome.tsx
+++ src/components/Slidehome/SlideHome.tsx
@@ -28,10 +28,18 @@ interface BannerItem {
 
 const SlideHome = ({ banner }: { banner: BannerItem[] }) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const labels = banner?.map((item) => item.name);
+
+  // Lọc các item hợp lệ (các item có `title`, `description`, hoặc `urlImage`)
+  const filteredBanner = banner.filter(
+    (item) =>
+      item.title || item.description || item.urlImage?.data?.attributes?.url
+  );
+
+  const labels = filteredBanner?.map((item) => item.name);
 
   const baseUrl = process.env.URL_API;
   const t = useTranslations("home");
+  console.log("Filtered banner", filteredBanner);
 
   return (
     <>
@@ -46,60 +54,53 @@ const SlideHome = ({ banner }: { banner: BannerItem[] }) => {
         modules={[EffectFade, Pagination, Autoplay]}
         className="swiper-home relative"
         onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}>
-        {banner?.map((item) => {
-          return (
-            <SwiperSlide key={item.id}>
-              <div className="image-container">
-                <Image
-                  src={
-                    `${baseUrl}${item.urlImage?.data?.attributes?.url}` || "/"
-                  }
-                  alt="Slide Home"
-                  layout="fill"
-                  // width={1000}
-                  // height={1000}
-                  priority
-                  objectFit="cover"
-                />
-                {/* asndashdaskdhlsandlasbkdjsalkdb */}
-                <div
-                  className={`content-baner flex justify-center desktop:items-center mobile:pt-[60px]`}>
-                  <div className="w-[90%] laptop:w-[846px]">
-                    <div className="grid grid-cols-1 gap-[32px]">
-                      <div className="col-span-1">
-                        <p className=" py-[4px]  text-white  w-fit border-b border-white">
-                          {item.name}
-                        </p>
-                      </div>
-                      <div className="col-span-1">
-                        <h2 className="text-[28px] laptop:text-[48px] mobile:text-[24px]  font-bold text-white line-clamp-2 laptop:min-h-[144px] tablet:min-h-[72px] mobile:min-h-[72px]">
-                          {item.title}
-                        </h2>
-                      </div>
-                      <div className="col-span-1">
-                        <p className="text-white tablet:text-[18px] mobile:text-[14px] line-clamp-2  laptop:min-h-[54px] tablet:min-h-[54px] mobile:min-h-[42px]">
-                          {item.description}
-                        </p>
-                      </div>
-                      <div className="col-span-1">
-                        <Link
-                          href={item.path ? item.path : "/"}
-                          className="rounded-[32px] py-[12px] font-bold px-[24px] bg-[#28A645] text-white border border-[#28A645] hover:text-white hover:bg-transparent transition-all 300ms">
-                          {t("see_more")}
-                        </Link>
-                      </div>
+        {filteredBanner?.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="image-container">
+              <Image
+                src={`${baseUrl}${item.urlImage?.data?.attributes?.url}` || "/"}
+                alt="Slide Home"
+                layout="fill"
+                priority
+                objectFit="cover"
+              />
+              <div
+                className={`content-baner flex justify-center desktop:items-center mobile:pt-[60px]`}>
+                <div className="w-[90%] laptop:w-[846px]">
+                  <div className="grid grid-cols-1 gap-[32px]">
+                    <div className="col-span-1">
+                      <p className="py-[4px] text-white w-fit border-b border-white">
+                        {item.name}
+                      </p>
+                    </div>
+                    <div className="col-span-1">
+                      <h2 className="text-[28px] laptop:text-[48px] mobile:text-[24px] font-bold text-white line-clamp-2 laptop:min-h-[144px] tablet:min-h-[72px] mobile:min-h-[72px]">
+                        {item.title}
+                      </h2>
+                    </div>
+                    <div className="col-span-1">
+                      <p className="text-white tablet:text-[18px] mobile:text-[14px] line-clamp-2 laptop:min-h-[54px] tablet:min-h-[54px] mobile:min-h-[42px]">
+                        {item.description}
+                      </p>
+                    </div>
+                    <div className="col-span-1">
+                      <Link
+                        href={item.path ? item.path : "/"}
+                        className="rounded-[32px] py-[12px] font-bold px-[24px] bg-[#28A645] text-white border border-[#28A645] hover:text-white hover:bg-transparent transition-all 300ms">
+                        {t("see_more")}
+                      </Link>
                     </div>
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-          );
-        })}
+            </div>
+          </SwiperSlide>
+        ))}
         <div className="flex justify-center items-center container">
           <div className="laptop:w-[846px] tablet:w-full container mobile:pr-[32px] mobile:pl-[32px] desktop:pl-0 absolute tablet:top-1/2 mobile:bottom-[86px] translate-y-[70%] z-30">
             <ProgressBar
               currentIndex={currentIndex}
-              stepsCount={banner?.length}
+              stepsCount={filteredBanner?.length}
               labels={labels}
             />
           </div>
